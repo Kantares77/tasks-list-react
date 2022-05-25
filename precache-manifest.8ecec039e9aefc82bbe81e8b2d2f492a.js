32776a1da6e9fd9e7f42fbd5d0deaaa6c8e79782
Marcin Kantarski <marcin.kantarski77@gmail.com>
2022-05-25 23:25:37 +0200

    Updates

---
+++ precache-manifest.8ecec039e9aefc82bbe81e8b2d2f492a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "83bc6bcfe27fbe516e92",
+    "url": "/tasks-list-react/static/css/main.385dbb31.chunk.css"
+  },
+  {
+    "revision": "83bc6bcfe27fbe516e92",
+    "url": "/tasks-list-react/static/js/main.4182f326.chunk.js"
+  },
+  {
+    "revision": "ee0bbfa4d7f9ad7bdf12",
+    "url": "/tasks-list-react/static/js/runtime~main.0c0668d3.js"
+  },
+  {
+    "revision": "e174e2884726cc3846b3",
+    "url": "/tasks-list-react/static/js/2.ae6cf435.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/tasks-list-react/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "cb9db7d53f99f4ea8e15f04bb0000889",
+    "url": "/tasks-list-react/index.html"
+  }
+];
