c9bcdec18c9963d8cfe7a4a7c901dc7beee0ecc9
Marcin Kantarski <marcin.kantarski77@gmail.com>
2022-06-22 21:54:41 +0200

    removeTask interaction

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-const tasks = [
-    { id: 1, content: "Przebiec maraton", done: false },
-    { id: 2, content: "Przebiec półmaraton", done: true },
-];
 
 function App() {
     const [hideDone, setHideDone] = useState(false);
+    const [tasks, setTasks] = useState([
+        { id: 1, content: "Przebiec maraton", done: false },
+        { id: 2, content: "Przebiec półmaraton", done: true },
+    ]);
 
     const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone);
+    };
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
     };
 
     return (
@@ -39,6 +43,7 @@ function App() {
                     <Tasks
                         tasks={tasks}
                         hideDone={hideDone}
+                        removeTask={removeTask}
                     />
                 }
             />
