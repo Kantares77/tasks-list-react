e3cbc57eb8c23dc201f1c1d776938f8784b40db3
Marcin Kantarski <marcin.kantarski77@gmail.com>
2022-06-22 22:51:57 +0200

    toggleTaskDone interaction

--- src/App.js
+++ src/App.js
@@ -22,6 +22,16 @@ function App() {
         setTasks(tasks => tasks.filter(task => task.id !== id));
     };
 
+    const toggelTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return {...task, done: !task.done};
+            }
+
+            return task;
+        }));
+    };
+
     return (
         <Container>
             <Header title="Lista zadań" />
@@ -44,6 +54,7 @@ function App() {
                         tasks={tasks}
                         hideDone={hideDone}
                         removeTask={removeTask}
+                        toggleTaskDone={toggelTaskDone}
                     />
                 }
             />
