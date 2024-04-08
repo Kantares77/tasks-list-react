3429df2158ea8f1f9f0bd187e63a2f1794c7b1fe
Marcin Kantarski <marcin.kantarski77@gmail.com>
2024-04-08 21:42:29 +0200

    implementation theme to app

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+        gallery: "#ebebeb",
+        white: "#ffffff",
+        seaGreen: "#319b3e",
+        malachite: "#18e732",
+        mintGreen: "#8Bfe99",
+        tamarillo: "#8b0e0e",     
+        thunderbird: "#c70f0f",       
+        seaPink: "#ed8c8c",
+    },
+    breakpoints: {
+        mobile: 767,
+    },
+};
