81aaaab6ff03efc3ca8083b5c952c93d1fdd29bc
Marcin Kantarski <marcin.kantarski77@gmail.com>
2024-04-08 21:40:17 +0200

    implementation of global styles to app

---
+++ src/globalStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        font-family: 'Montserrat', sans-serif;
+        word-break: break-word;
+        background: hsl(0deg 0% 92%);
+    }
+
+    button {
+        cursor: pointer;
+    }
+`;
+
+export const Container = styled.main`
+    max-width: 850px;
+    padding: 20px;
+    margin: auto;
+    margin-top: 10px;    
+`;
