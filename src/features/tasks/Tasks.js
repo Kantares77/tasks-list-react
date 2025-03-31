6918050723e82fa31b8d6e45cba983a7a7a55b76
Marcin Kantarski <marcin.kantarski77@gmail.com>
2025-03-31 20:26:03 +0200

    remove useTask, selectTasks and useSelector form Tasks.js

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,46 +1,24 @@
-import { useSelector } from "react-redux";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import { Container } from "../../globalStyles";
 import Header from "../../common/Header";
-import { useTasks } from "../../useTasks";
-import { selectTasks } from "./tasksSlice";
-
 
 function Tasks() {
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    //tasks,   
-    removeTask,
-    toggleTaskDone,
-    markAllDone,
-    addNewTask,
-  } = useTasks();
 
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
+
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            //tasks={tasks}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderButtons={
-          <Buttons
-            markAllDone={markAllDone}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderButtons={<Buttons />}
       />
     </Container>
   );
