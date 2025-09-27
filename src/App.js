a47b08eb02164a3c8cd1d3f7221983f39f574dfe
Kantares77 <marcin.kantarski77@gmail.com>
2025-09-27 19:32:23 +0200

    install react-router-dom & AuthorPage.js added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
-import React from "react";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import AuthorPage from "./features/author/AuthorPage";
 
-export const App = () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/zadania">Zadania</Link>
+                </li>
+                <li>
+                    <Link to="/autor">O autorze</Link>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Redirect to="/zadania" />
+            </Switch>
+        </nav>
+    </HashRouter>
+);
---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,31 @@
+import { Container } from "../../globalStyles";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+
+export default () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Kantares"
+            body={
+                <>
+            <p>
+                Pomimo faktu, iż zespół <strong>Coma</strong> zawieśił swoją działalność jest to zdecydowanie jeden z najlepszych bandów rockowych ostatnich lat jaki powstał w Polsce.
+            </p>
+            <p>
+                Zespół został założony w 1998 roku w
+                        Łodzi
+                        przez gitarzystę Dominika Witczaka i perkusistę Tomasza Stasiaka, którzy wcześniej byli związani
+                        z
+                        grupą Ozoz. Z czasem do formacji dołączyli basista Rafał Matuszak, gitarzysta Wojciech Grenda i
+                        wokalista Piotr Rogucki. W 2001 Grenda został zastąpiony przez Marcina Kobzę, po czym zespół
+                        dotarł
+                        do ostatniego etapu przesłuchań konkursu debiutów w Opolu. Dwa lata później podpisał kontrakt z
+                        BMG
+                        Poland.
+            </p>
+            </>
+            }
+        />
+    </Container>
+);
