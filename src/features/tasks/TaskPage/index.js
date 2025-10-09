9a9ef123b6479a41e6ef7374850f6ae4ebfcd6e1
Kantares77 <marcin.kantarski77@gmail.com>
2025-10-09 21:43:15 +0200

    copy from "virgin file" content of index.js

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,25 @@
+import Section from "../../../common/Section";
+import { Container } from "../../../globalStyles";
+import Header from "../../../common/Header";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
+
+function TaskPage() {
+    const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
+
+    return (
+        <Container>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={task ? task.content : "Nie znaleziono zadania 🥹"}
+                body={
+                    <><strong>Ukończono: </strong>{task.done ? "Tak" : "Nie"}</>
+                }
+            />
+        </Container>
+    );
+}
+
+export default TaskPage;
