ea1603e5d46444152d757f145bae04678895e294
Kantares77 <marcin.kantarski77@gmail.com>
2025-10-09 22:00:33 +0200

    change of index.js in "Form" component

--- src/features/tasks/TasksPage/Form/index.js
+++ src/features/tasks/TasksPage/Form/index.js
@@ -5,19 +5,11 @@ import { StyledButton, StyledForm } from "./styled";
 import { addTask } from "../../tasksSlice";
 import Input from "../../Input";
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 50f717d03c6ec2656a2ac513e53b6a8c7e2cc637
 const Form = () => {
     const [newTaskContent, setNewTaskContent] = useState("");
     const inputRef = useRef(null);
 
-<<<<<<< HEAD
     const dispatch = useDispatch()
-=======
-    const dispatch = useDispatch();
->>>>>>> 50f717d03c6ec2656a2ac513e53b6a8c7e2cc637
 
     const onFormSubmit = (event) => {
         event.preventDefault();
