1de53a35bb1726c46b1b9c73b637a0ae47feb43a
Kantares77 <marcin.kantarski77@gmail.com>
2025-10-12 17:39:16 +0200

    applied new hook "useQueryParameter" and use it in relevant components

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,23 +1,30 @@
-import { useLocation, useHistory } from "react-router-dom";
+//import { useLocation, useHistory } from "react-router-dom";
 import Input from "../../Input";
 import { Wrapper } from "./styled";
 import searchQueryParamName from "../searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
 
 export default () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    // const location = useLocation();
+    // const history = useHistory();
+    //const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
+        // const searchParams = new URLSearchParams(location.search);
 
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
+        // if (target.value.trim() === "") {
+        //     searchParams.delete(searchQueryParamName);
+        // } else {
+        //     searchParams.set(searchQueryParamName, target.value);
+        // }
 
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        // history.push(`${location.pathname}?${searchParams.toString()}`);
     };
 
     return (
